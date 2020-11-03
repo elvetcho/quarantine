9d9fb62b56c7b26abde2e78d9ea5a8830aebf5fa
Hervé AKAKPO <h.akakpo@groupeonepoint.com>
2020-11-03 12:34:59 +0100

    first push

---
+++ src/main/java/com/onepoint/exercises/Quarantine.java
@@ -0,0 +1,31 @@
+package com.onepoint.exercises;
+
+public class Quarantine {
+    public Quarantine(String changeme) {
+        throw new RuntimeException("NotYetImplementedException");
+    }
+
+    public String report() {
+        throw new RuntimeException("NotYetImplementedException");
+    }
+
+    public void wait40Days() {
+        throw new RuntimeException("NotYetImplementedException");
+    }
+
+    public void aspirin() {
+        throw new RuntimeException("NotYetImplementedException");
+    }
+
+    public void antibiotic() {
+        throw new RuntimeException("NotYetImplementedException");
+    }
+
+    public void insulin() {
+        throw new RuntimeException("NotYetImplementedException");
+    }
+
+    public void paracetamol() {
+        throw new RuntimeException("NotYetImplementedException");
+    }
+}
